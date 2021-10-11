83c0976ad8b4d1705c150fd78def1f33fe1f1987
Andre Staltz <andre@staltz.com>
2021-10-11 16:18:17 +0300

    fork the repo for ssb-ngi-pointer benchmarks

--- ios/Manyverse/BuildConfig.h
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2021 The Manyverse Authors
-//
-// SPDX-License-Identifier: MPL-2.0
-
-#import <React/RCTBridgeModule.h>
-
-@interface BuildConfig : NSObject<RCTBridgeModule>
-
-@end
